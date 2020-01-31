562a5d8ed30581045ba2d7dd7ad179459c576f9c
Peter Alexandre <petermiguel99@gmail.com>
2020-01-30 21:14:55 -0300

    Update models using new diagram class definition

--- justdoit/todolist/models.py
+++ justdoit/todolist/models.py
@@ -1,65 +1,45 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
+from justdoit.todolist import choices
 
 
 class BaseModel(models.Model):
-    title = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
 
-class Board(BaseModel):
 
-    users = models.ManyToManyField(User)
+class ToDo(BaseModel):
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'board'
-        verbose_name_plural = 'boards'
-
-
-class ColumnList(BaseModel):
-
-    user_board = models.ForeignKey(
-        'Board',
-        on_delete=models.CASCADE,
-        related_name='lists'
-    )
-    position = models.PositiveIntegerField()
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'list'
-        verbose_name_plural = 'lists'
+        verbose_name = 'Todolist'
+        verbose_name_plural = 'Todolists'
 
 
 class Task(BaseModel):
-
-    ACTIVE = 'ACTIVE'
-    ARCHIVED = 'ARCHIVED'
-    DONE = 'DONE'
-
-    STATUS_CHOICES = [
-        (ACTIVE, 'Active'),
-        (ARCHIVED, 'Archived'),
-        (DONE, 'Done'),
-    ]
-
+    title = models.CharField(max_length=60)
     description = models.TextField()
-    due_date = models.DateTimeField(null=True)
-    status = models.CharField(
-        max_length=8, choices=STATUS_CHOICES, default=ACTIVE)
-    position = position = models.PositiveIntegerField()
-    column_list = models.ForeignKey(
-        'ColumnList', on_delete=models.CASCADE, null=True)
+    deadline = models.DateTimeField(blank=True)
+    status = models.CharField(max_length=1, choices=choices.TaskStatus, default='OPEN')
+    to_do = models.ForeignKey('ToDo', on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', related_name='tasks', related_query_name='task', verbose_name='Tags')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'task'
-        verbose_name_plural = 'tasks'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+
+class Tag(BaseModel):
+    title = models.CharField(max_length=20)
+    color = models.CharField(max_length=7, choices=choices.TagColor, default='GRA')
