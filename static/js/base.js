bdefd61b8de66afb00570e51dbc4871ffcbd4e02
Peter Alexandre <petermiguel99@gmail.com>
2020-02-20 23:33:19 -0300

    Update sidebar form to create todolist using API

---
+++ static/js/base.js
@@ -0,0 +1,18 @@
+function fromCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function safeMethod(method) {
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
