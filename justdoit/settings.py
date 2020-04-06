f27458b78c6a0e555f6d605f0b387044b40f401f
Peter Alexandre <petermiguel99@gmail.com>
2020-04-06 08:02:39 -0300

    Add whitenoise to load statics in webserver

--- justdoit/settings.py
+++ justdoit/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,7 @@ STATIC_ROOT = BASE_DIR('staticfiles')
 STATICFILES_DIRS = (
     BASE_DIR('static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR('mediafiles')
 MEDIA_URL = '/media/'
